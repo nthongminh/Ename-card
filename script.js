b67c249ec7cfa8bb21a3d90927ee1e23100293af
truongginjs <truongginjs@gmail.com>
2023-10-10 19:17:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// clickHandle()
+function clickHandle() {
+    window.open(`https://www.google.com`)
+}
+
+function iff(condition, True, False) {
+    if (condition) {
+        alert(True)
+        return True;
+    }
+    else {
+        // alert(False)
+        return False;
+    }
+}
+
+var b = 2
+const a = ''
+let c = 12
+
+// a='zxc'
+b = 22
+c = 122
+
+const d = b+c;
+
+
+
+iff(1 == 1, 'True', 'False')
+
+// =if(1=1,'true','false')
